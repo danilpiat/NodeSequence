a783616479969f734dab9320875ed71619c39112
danilpiat <56876348+danilpiat@users.noreply.github.com>
2019-12-07 17:10:31 +0300

    Add files via upload

---
+++ ConsoleApplication1/NodeSequence.cpp
@@ -0,0 +1,274 @@
+
+
+#include "Sequence.h"
+#include "NodeSequence.h"
+#include "SortClass.h"
+#include <iostream>
+#include "Animal.h"
+#include <time.h> 
+
+void testing_sorts()
+{
+	std::cout << "Starting tests of sorting methods" << std::endl;
+	time_t start, end;
+	SortClass B;
+	NodeSequence<int> a;
+	for (int i = 0; i < 10000; i++)
+	{
+		a.addelem(rand() % 50 - rand() % 50);
+	}
+	std::cout << "Testing bublesort on 10000 elements" << std::endl;
+	time(&start);
+	B.bubleSort(a, a.getLength(), [](const int& first, const int& second) -> bool
+		{
+			return second > first;
+		});
+	time(&end);
+	double seconds = difftime(end, start);
+	std::cout << "The time: " << seconds << " seconds" << std::endl;
+	NodeSequence<int> b;
+	std::cout << "Testing insert_sort on 10000 elements" << std::endl;
+	for (int i = 0; i < 10000; i++)
+	{
+		b.addelem(rand() % 50 - rand() % 50);
+	}
+	time(&start);
+	B.insert_sort(b, b.getLength(), [](const int& first, const int& second) -> bool
+		{
+			return second > first;
+		});
+	time(&end);
+	seconds = difftime(end, start);
+	std::cout << "The time: " << seconds << " seconds" << std::endl;
+	NodeSequence<int> c;
+	std::cout << "Testing shell_sort on 10000 elements" << std::endl;
+	for (int i = 0; i < 10000; i++)
+	{
+		b.addelem(rand() % 50 - rand() % 50);
+	}
+	time(&start);
+	B.shellSort(b, b.getLength(), [](const int & first, const int & second) -> bool
+		{
+			return second > first;
+		});
+	time(&end);
+	seconds = difftime(end, start);
+	std::cout << "The time: " << seconds << " seconds" << std::endl;
+
+	NodeSequence<int> a1;
+	for (int i = 0; i < 25000; i++)
+	{
+		a1.addelem(rand() % 50 - rand() % 50);
+	}
+	std::cout << "Testing bubleSort on 25000 elements" << std::endl;
+	time(&start);
+	B.bubleSort(a1, a1.getLength(), [](const int& first, const int& second) -> bool
+		{
+			return second > first;
+		}); // (a1, a1.getLength(), a1.is_smaller);
+	time(&end);
+	seconds = difftime(end, start);
+	std::cout << "The time: " << seconds << " seconds" << std::endl;
+	std::cout << "Testing insert_sort on 25000 elements" << std::endl;
+	NodeSequence<int> b1;
+	for (int i = 0; i < 25000; i++)
+	{
+		b1.addelem(rand() % 50 - rand() % 50);
+	}
+	time(&start);
+	/*B.insert_sort(b1, b1.getLength(), [](NodeSequence<int>& a, int i, int j) -> bool
+		{
+			if (a[i] < a[j])
+				return true;
+			else
+				return false;
+		});*/
+	time(&end);
+	seconds = difftime(end, start);
+	std::cout << "The time: " << seconds << " seconds" << std::endl;
+	std::cout << "Testing shell_sort on 25000 elements" << std::endl;
+	NodeSequence<int> c1;
+	for (int i = 0; i < 25000; i++)
+	{
+		b1.addelem(rand() % 50 - rand() % 50);
+	}
+	time(&start);
+	B.shellSort(b1, b1.getLength(), [](NodeSequence<int>& a, int i, int j) -> bool
+		{
+			if (a[i] < a[j])
+				return true;
+			else
+				return false;
+		});
+	time(&end);
+	seconds = difftime(end, start);
+	std::cout << "The time: " << seconds << " seconds" << std::endl;
+
+	std::cout << "Testing bubleSort on 50000 elements" << std::endl;
+	NodeSequence<int> a2;
+	for (int i = 0; i < 50000; i++)
+	{
+		a2.addelem(rand() % 50 - rand() % 50);
+	}
+	time(&start);
+	/*B.bubleSort(a2, a2.getLength(), [](NodeSequence<int>& a, int i, int j) -> bool
+		{
+			if (a[i] < a[j])
+				return true;
+			else
+				return false;
+		});*/
+	time(&end);
+	seconds = difftime(end, start);
+	std::cout << "The time: " << seconds << " seconds" << std::endl;
+	std::cout << "Testing insert_sort on 50000 elements" << std::endl;
+	NodeSequence<int> b2;
+	for (int i = 0; i < 50000; i++)
+	{
+		b2.addelem(rand() % 50 - rand() % 50);
+	}
+	time(&start);
+	/*B.insert_sort(b2, b2.getLength(), [](NodeSequence<int>& a, int i, int j) -> bool
+		{
+			if (a[i] < a[j])
+				return true;
+			else
+				return false;
+		});*/
+	time(&end);
+	seconds = difftime(end, start);
+	std::cout << "The time: " << seconds << " seconds" << std::endl;
+	std::cout << "Testing shell_sort on 50000 elements" << std::endl;
+	NodeSequence<int> c2;
+	for (int i = 0; i < 50000; i++)
+	{
+		b2.addelem(rand() % 50 - rand() % 50);
+	}
+	time(&start);
+	B.shellSort(b2, b2.getLength(), [](NodeSequence<int>& a, int i, int j) -> bool
+		{
+			if (a[i] < a[j])
+				return true;
+			else
+				return false;
+		});
+	time(&end);
+	seconds = difftime(end, start);
+	std::cout << "The time: " << seconds << " seconds" << std::endl;
+}
+
+
+//template<typename TElement>
+int main()
+{
+	testing_sorts();
+	SortClass B;
+	NodeSequence<int> a;
+	
+	/*B.insert_sort(a, a.getLength(), [](NodeSequence<int>& a, int i, int j) -> bool
+		{
+			if (a[i] < a[j])
+				return true;
+			else
+				return false;
+		});*/
+	/*B.bubleSort(a, a.getLength(), [](NodeSequence<int>& a, int i, int j) -> bool
+		{
+			if (a[i] > a[j])
+				return true;
+			else
+				return false;
+		});*/
+	B.shellSort(a, a.getLength(), [](NodeSequence<int>& a, int i, int j) -> bool
+		{
+			if (a[i] > a[j])
+				return true;
+			else
+				return false;
+		});
+	std::cout << "Len : " << a.getLength() << "Full: " << a.getIsEmpty() << std::endl;
+	a.printList();
+	a.deletelem(0);
+	std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+
+	std::cout << "Element by operator " << a[2] << std::endl;
+	a[1] = 0;
+	a.printList();
+	std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+
+	a.printList();
+	try
+	{
+		a.deletelem(5);
+	}
+	catch (const std::exception & ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+	std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+	a.printList();
+	a.deletelem(0);
+	std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+	std::cout << "Len : " << a.getLength() << std::endl;
+	a.printList();
+	try
+	{
+		a.deletelem(2);
+	}
+	catch (const std::exception & ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+	std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+	std::cout << "Len : " << a.getLength() << std::endl;
+	a.printList();
+
+	int b1(32);
+	int b2(4);
+	int b3(8);
+	int b4(12);
+	int b5(23);
+	int b6(55);
+	int b7(70);
+	int b8(1);
+	NodeSequence<int> a2;
+
+	a2.addelem(b1);
+	a2.addelem(b2);
+	a2.addelem(b3);
+	a2.addelem(b4);
+	a2.addelem(b5);
+	a2.addelem(b6);
+	a2.addelem(b7);
+	a2.addelem(b8);
+
+	/*B.bubleSort(a2, a2.getLength(), [](NodeSequence<int>& a, int i, int j) -> bool
+		{
+			if (a[i] < a[j])
+				return true;
+			else
+				return false;
+		});*/
+	a2.printList();
+	
+	a2.printList();
+	std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+	try
+	{
+		a2.deletelem(-4);
+	}
+	catch (const std::exception & ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+
+	a2.deletelem(4);
+	a2.printList();
+	std::cout << "Len : " << a2.getLength() << "Full: " << a2.getIsEmpty() << std::endl;
+	std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+
+	a2.deletelem(1);
+	a2.printList();
+
+
+}
---
+++ ConsoleApplication1/SortClass.h
@@ -0,0 +1,99 @@
+#pragma once
+#include "Sequence.h"
+
+
+class SortClass 
+{
+public:
+	SortClass();
+	~SortClass();
+	template<typename TElement, template <typename ...> class Container, class Compare>
+	void bubleSort(Container<TElement>& array, int size, Compare comp);
+	template<typename TElement, template <typename ...> class Container, class Compare>
+	void shellSort(Container<TElement>& vector, int len, Compare comp);
+	template<typename TElement, template <typename ...> class Container, class Compare>
+	void insert_sort(Container<TElement>& vector, int size, Compare comp);
+	
+
+
+	
+};
+
+template<typename TElement, template <typename ...> class Container, class Compare>
+void SortClass::bubleSort(Container<TElement>& array, int size, Compare comp)
+{
+	/*TElement tmp;
+	int k;
+	while (size > 1)
+	{
+		k = 0;
+		for (int i = 1; i < size; i++)
+		{
+			if (comp(array, i, i - 1))
+			{
+				tmp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = tmp;
+				k = i;
+			}
+		}
+		size = k;
+	}*/
+
+	auto head = array.get(0);
+	while (head->next != nullptr)
+	{
+		head = head->next;
+		auto curHead = head;
+		while (curHead->next != nullptr)
+		{
+
+			if (comp(head->data, curHead->data))
+			{
+				auto temp = head->data;
+				head->data = curHead->data;
+				curHead->data = temp;
+			
+			}
+			curHead = curHead->next;
+		}
+	}
+}
+
+template<typename TElement, template <typename ...> class Container, class Compare>
+void  SortClass::insert_sort(Container<TElement>& vector, int size, Compare comp)
+{
+	
+	/*for (int i = 1; i < size; i++)
+	{
+		TElement cur = vector[i];
+		int j = i;
+		while (j > 0 && comp(vector, i, j - 1))
+		{
+			vector[j] = vector[j - 1];
+			j--;
+		}
+		vector[j] = cur;
+	}*/
+}
+
+template<typename TElement, template <typename ...> class Container, class Compare>
+void  SortClass::shellSort(Container<TElement> &vector, int len, Compare comp)
+{
+	int step = len / 2;
+	while (step > 0)
+	{
+		int i, j;
+		for (i = step; i < len; i++)
+		{
+			TElement value = vector[i];
+			for (j = i - step; (j >= 0) && (vector[j] > value); j -= step)
+				vector[j + step] = vector[j];
+			vector[j + step] = value;
+		}
+		step /= 2;
+	}
+}
+
+
+
